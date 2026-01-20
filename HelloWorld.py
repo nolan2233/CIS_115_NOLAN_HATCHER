f987b12d62f239d2168190bf28e0958cd7e0d990
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-01-20 09:45:39 -0500

    my first program modified

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
+#This is my first Python program 
 
+print("Hello World!")
+
+num1 = input("Enter First Number: ")
+num2 = input("Enter Second Number: ")
+sum = float(num1) + float(num2)
+
+print (sum) 
