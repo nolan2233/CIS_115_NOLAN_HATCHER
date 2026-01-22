b8bedd3b6e9e01d842049acc50149e6325248c97
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-01-22 09:50:38 -0500

    1/22code

---
+++ devideByZero.py
@@ -0,0 +1,8 @@
+#This is my program to divide by zero
+
+numerator = 10
+denominator = 0 
+
+result =numerator / denominator
+
+print (f'My result is: {result}')
---
+++ multiplyTwoNumbers.py
@@ -0,0 +1,8 @@
+#This is my program to multiply two numbers
+
+num1 = 70
+num2 = 11
+
+result = num1 * num2
+
+print (f'My result value is: {result}')
