eb322a44445d7a91713e47d125e7a8a9c93bae90
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-02-18 09:42:49 -0500

    2/18

--- triFunc.py
+++ triFunc.py
@@ -1,4 +1,12 @@
 #this program uses functions to calculate hypoteneuse of a right triangle
 
 def triangle():
-    
+
+    side1 = int(input("Enter first number:"))
+    side2 = int(input("Enter second number:"))
+    product = (side1 * side2) + (side2 * side2) 
+    side3 = product ** .5
+
+    print(f"The missing side is: {side3}")
+
+triangle()
