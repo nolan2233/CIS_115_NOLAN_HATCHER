a921ef7a1fa31395d6b5a0c50139cee69c58d77b
= <=>
2026-02-25 14:59:18 -0500

    2/25

--- kinetic_energy.py
+++ kinetic_energy.py
@@ -4,7 +4,7 @@ mass = int(input("Enter the value for mass: "))
 velocity = int(input("Enter the value for velocity: "))
 
 def kinetic_energy(mass,velocity):
-    KE = .5 * (mass * (velocity ** 2))
-    print(f'The amount of kinetic energy the ogject has is: {KE}kgs/mps2')
+    kenetic = .5 * (mass * (velocity ** 2))
+    print(f'The amount of kinetic energy the ogject has is: {kenetic}kgs/mps2')
 
 kinetic_energy(mass,velocity)
